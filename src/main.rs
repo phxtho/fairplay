f3fea48bf8cd9c3cf80ef816343a4d9f25bd3c17
phxtho <phathopukwana@gmail.com>
2023-03-08 23:08:49 +0100

    capture frame a write to socket

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
-use http_body_util::Full;
-use hyper::{body::Bytes, server::conn::http1, service::service_fn, Request, Response};
-use std::{convert::Infallible, net::SocketAddr};
-use tokio::net::TcpListener;
+use std::net::SocketAddr;
+use tokio::{
+    io::AsyncWriteExt,
+    net::{TcpListener, TcpStream},
+};
 
 use hostname::get;
 use libmdns::Responder;
@@ -15,6 +16,45 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let listener = TcpListener::bind(addr).await?;
     println!("Listening on http://{}", addr);
 
+    broadcast_service();
+
+    // loop to continuously accept incoming connections
+    loop {
+        let (stream, _) = listener.accept().await?;
+        println!(
+            "Accepted connection from: {:?}",
+            stream.peer_addr().unwrap()
+        );
+
+        capture(stream).await;
+    }
+}
+
+async fn capture(mut stream: TcpStream) {
+    use scrap::{Capturer, Display};
+    use std::io::ErrorKind::WouldBlock;
+    let d = Display::primary().unwrap();
+    let (w, h) = (d.width(), d.height());
+
+    let mut capturer = Capturer::new(d).unwrap();
+
+    loop {
+        match capturer.frame() {
+            Ok(frame) => {
+                stream.write_all(&frame).await.unwrap();
+            }
+            Err(ref e) if e.kind() == WouldBlock => {
+                // Wait for the frame.
+            }
+            Err(_) => {
+                // We're done here.
+                break;
+            }
+        }
+    }
+}
+
+fn broadcast_service() {
     // Get the hostname of the device
     let hostname = get().unwrap().into_string().unwrap();
 
@@ -34,18 +74,4 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
         "Registered {:?} with mdns responder under {:?}",
         hostname, SERVICE_TYPE
     );
-
-    // loop to continuously accept incoming connections
-    loop {
-        let (stream, _) = listener.accept().await?;
-        println!(
-            "Accepted connection from: {:?}",
-            stream.peer_addr().unwrap()
-        );
-        // Spawn a tokio task to serve multiple connections concurrently
-        tokio::task::spawn(async move {
-            // Bind the incoming connection to our service
-            stream.try_write(buf)
-        });
-    }
 }
